87ee03280bad2594083595cc8d3b9c8a6b779806
Matthias Gabriel <matthias.gabriel@etit.tu-chemnitz.de>
2020-08-04 16:34:29 +0200

    don't install to lib64

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,7 @@ cd build
 cmake ../cmake_unofficial \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DBUILD_SHARED_LIBS=ON \
+      -DCMAKE_INSTALL_LIBDIR=lib \
       -DCMAKE_BUILD_TYPE=Release
 
 make -j ${CPU_COUNT}
