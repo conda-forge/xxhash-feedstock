94a2d574b0c4d25fd74958a8efdf19c12b453ad5
Matthias Gabriel <matthias.gabriel@etit.tu-chemnitz.de>
2020-08-04 16:27:08 +0200

    build using cmake

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,12 @@
-make all
-make check test-xxhsum-c namespaceTest
+#!/bin/sh
+
+mkdir build
+cd build
+
+cmake ../cmake_unofficial \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DBUILD_SHARED_LIBS=ON \
+      -DCMAKE_BUILD_TYPE=Release
+
+make -j ${CPU_COUNT}
 make install
