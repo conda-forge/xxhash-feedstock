0251dde8303a2a9549ddd5b1f316a742c09719f3
Marcelo Duarte Trevisani <marcelotrevisani@users.noreply.github.com>
2020-08-04 13:47:54 +0100

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,8 @@
-make all
-make check test-xxhsum-c namespaceTest
-make install
+mkdir build
+cd build
+cmake ../cmake_unofficial ^
+      -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+      -DBUILD_SHARED_LIBS=ON ^
+      -DCMAKE_BUILD_TYPE=Release
+cmake --build .
+cmake --build . --target install 
